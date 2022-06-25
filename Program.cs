2ab8413a859eff9d40d9b888be5b274fb31a742c
Elvira <rostechno_rb@mail.ru>
2022-06-25 15:25:48 +0500

    первый коммит

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Дано двузначное натуральное число.
+//Верно ли, что квадрат суммы его цифр больше самого числа
+
+//Console.WriteLine("Введите число");
+//int  = Convert.ToInt32(Console.ReadLine());
+//if (
+
+
+//Дана строка. Подсчитать количество содержащихся в ней цифр
+//string newString = "125_tywerd_ef125we";
+
+string newString;
+newString = Console.ReadLine();
+if (newString != null)
+{
+    int count = 0;
+
+    foreach (var i in newString) if (char.IsDigit(i)) count++;
+    Console.WriteLine(count);
+}
---
+++ readme.md.txt
@@ -0,0 +1,2 @@
+# Test
+proverka raboti c git
